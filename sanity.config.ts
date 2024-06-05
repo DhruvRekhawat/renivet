8732fdd750792176de51625caa918f1fa1eb9bcf
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-06-05 15:58:08 +0530

    mend

--- sanity.config.ts
+++ sanity.config.ts
@@ -18,10 +18,5 @@ export default defineConfig({
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+  
 })
