170fe8b68ea4c2740dcf1f4e70c463df8a482e0f
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-06-13 12:41:30 +0530

    logo

--- components/molecules/navbar.tsx
+++ components/molecules/navbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import Logo from '@/assests/icons/PrimaryLogo.svg'
+import Logo from '@/assests/icons/SecondaryLogo-Center.svg'
 import { Heart } from "lucide-react"
 import Link from "next/link"
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="bg-black text-white text-sm flex h-8 justify-center items-center w-full p-2">We Are Coming Soon <Heart className="text-white h-4"></Heart> </div>
     <nav className=" flex h-14 w-full items-center justify-between p-4 bg-white">
       
-        <Image src={Logo} alt="logo" height={80}></Image>
+        <Image src={Logo} alt="logo" height={40}></Image>
         <ul className="flex gap-4 text-md font-bold hover:cursor-pointer font-sans">
             <Link href='#howitworks'><li>How it Works</li></Link>
             <Link href='#subscribe'><li>Subscribe</li></Link>
